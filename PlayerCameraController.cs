57da17cc20b1c0b290eaf55ce5f2c85641dd5182
KevinChaves65 <64420490+KevinChaves65@users.noreply.github.com>
2020-05-13 20:53:59 -0400

    Add files via upload

---
+++ PlayerCameraController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private float lookSensitivity = 1f;
+
+    [SerializeField] private float smoothing = 1f;
+
+    private Vector3 basecamh;
+
+    private Vector3 crouchcamh;
+
+    public float CrouchHeight = .9f;
+
+    private GameObject player;
+
+    private Vector2 smoothedVelocity;
+
+    private Vector2 currentLookingPos;
+
+    private void Awake()
+    {
+        basecamh = transform.localPosition;
+        crouchcamh = transform.localPosition;
+        crouchcamh.y = CrouchHeight;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        Vector2 inputValues = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        inputValues = Vector2.Scale(inputValues, new Vector2(lookSensitivity * smoothing, lookSensitivity * smoothing));
+
+        smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValues.x, 1f / smoothing);
+        smoothedVelocity.y = Mathf.Lerp(smoothedVelocity.y, inputValues.y, 1f / smoothing);
+
+        currentLookingPos += smoothedVelocity;
+        currentLookingPos.y = Mathf.Clamp(currentLookingPos.y, -80f, 80f);
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLookingPos.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(currentLookingPos.x, player.transform.up);
+    }
+
+    private void cameracrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            player.transform.localPosition = crouchcamh;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            player.transform.localPosition = basecamh;
+        }
+    }
+}
+
---
+++ PlayerMovementController.cs
@@ -0,0 +1,169 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private float jumpraycastDistance = 1.6f;
+    [SerializeField] private float sprintspeed = 2f;
+    [SerializeField] private float climbingspeed = 2f;
+
+    private Rigidbody rb;
+
+    // Crouch
+    private Vector3 standColliderCenter;
+    private float slideforce = 5f;
+    private CapsuleCollider playerCollider;
+    private float standingSize;
+
+    public float fallmulti = 3f;
+    private float currentspeed = 0f;
+    
+    private bool isClimbing = false;
+    private bool doubleJump = false;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void Start()
+    {
+        standingSize = playerCollider.height;
+        standColliderCenter = playerCollider.center;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (IsGrounded() == false && doubleJump == true)
+        {
+            Jump();
+            doubleJump = false;
+        }
+
+        if(IsGrounded())
+        {
+            Jump();
+            doubleJump = true;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallmulti - 1) * Time.deltaTime;
+        }
+        crouch();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentspeed = sprintspeed;
+        }
+        else
+        {
+            currentspeed = speed;
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing==false)
+        {
+            Move();
+        }
+        if (isClimbing==true)
+        {
+            Climb();
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    private void Move()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis).normalized ;
+
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement * currentspeed * Time.fixedDeltaTime);
+
+        rb.MovePosition(newPosition);
+    }
+
+    private bool IsGrounded()
+    {
+        //Debug.DrawRay(transform.position, Vector3.down * jumpraycastDistance, Color.red);
+        return Physics.Raycast(transform.position, Vector3.down, jumpraycastDistance);
+    }
+
+    private void crouch()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            playerCollider.height = standingSize / 1.5f;
+            playerCollider.center = standColliderCenter / 2;
+
+
+            slide();
+        }
+
+        if(Input.GetKeyUp(KeyCode.C))
+        {
+            playerCollider.height = standingSize;
+            playerCollider.center = standColliderCenter;
+        }
+
+    }
+
+    private void slide()
+    {
+        if (rb.velocity.magnitude > 0.5f)
+        {
+            if (IsGrounded())
+            {
+                rb.AddForce(Vector3.forward * slideforce);
+            }
+        }
+
+    }
+    //to be actually done (or not idk :( )
+    private void Climb()
+    {
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 upforce = new Vector3(0, vAxis, 0);
+        Vector3 newPos = rb.position + rb.transform.TransformDirection(upforce * climbingspeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+    //wtf is wrong with this
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Climable")
+        {
+            isClimbing = true;
+            Debug.Log("Gay");
+        }
+    }
+    void OnCollisionExit(Collider col)
+    {
+        if (col.gameObject.tag == "Climbable")
+        {
+            isClimbing = false;
+            Debug.Log("Shit");
+        }
+    }
+}
